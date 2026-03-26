7100d4ecec9f569b776f92f4a78b2bb9636d25c4
Abhijeet <abhijeetxenium@email.com>
2026-03-26 14:22:30 +0530

    updated sqls

--- sqls/output/sales.sql
+++ sqls/output/sales.sql
@@ -1,4 +1,3 @@
-create or replace table `airflow-data-pipelines-491407.analytics.sales` AS
 SELECT
   *
 FROM `airflow-data-pipelines-491407.analytics.sales_transform`
--- sqls/trans/sales_transform.sql
+++ sqls/trans/sales_transform.sql
@@ -1,3 +1,2 @@
-create or replace table `airflow-data-pipelines-491407.analytics.sales_transform` AS
 SELECT *
 FROM `airflow-data-pipelines-491407.analytics.sales_raw`
